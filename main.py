c3cd2395004ff82714781810480bee046d1d0147
Jon Craton <jncraton@gmail.com>
2025-07-10 13:40:20 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from pybricks.hubs import CityHub
+from pybricks.pupdevices import DCMotor, Light
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = CityHub()
+
