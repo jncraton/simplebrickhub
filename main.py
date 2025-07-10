1a62cc57e49512ea0ef3130139b47c8c2fa73cc9
Jon Craton <jncraton@gmail.com>
2025-07-10 13:53:36 -0400

    Add light toggle

--- main.py
+++ main.py
@@ -7,7 +7,27 @@ from umath import sin, pi
 
 hub = CityHub()
 
+# Disable the stop button.
+hub.system.set_stop_button(None)
+
 # Animate hub light
 hub.light.animate([Color.WHITE * (0.5 * sin(i / 15 * pi) + 0.5) for i in range(30)], 40)
 
-wait(10000)
+lightA = Light(Port.A)
+lightB = Light(Port.B)
+
+lightA.on(100)
+lightB.on(100)
+
+a_on = True
+
+while True:
+    if hub.buttons.pressed():
+        if a_on:
+            lightA.off()
+            lightB.on(100)
+        else:
+            lightA.on(100)
+            lightB.off()
+        a_on = not a_on
+        wait(200)
